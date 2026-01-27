eb113b9d4b7dd41e65ad41d6e349abace2221082
rspence821505 <rspence1505@gmail.com>
2026-01-26 20:06:57 -0600

    feat: ran all imported notebooks except for inflation

--- src/filters/__init__.py
+++ src/filters/__init__.py
@@ -6,12 +6,13 @@ from .base_filter import BaseFilter
 from .seq_enkf import StochasticEnKF
 from .enkf_4d import StochasticEnKF4D
 from .qpca_endcf import QPCAEnDCF
-from .letkf import LETKF
+
+# from .letkf import LETKF
 
 __all__ = [
     "BaseFilter",
     "StochasticEnKF",
     "StochasticEnKF4D",
     "QPCAEnDCF",
-    "LETKF",
+    # "LETKF",
 ]
